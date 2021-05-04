98bf9a6355ab38f54fd4f7d9c458de4c073ba599
apolenary20 <71891178+apolenary20@users.noreply.github.com>
2021-05-04 18:06:52 +0300

    Update Ph.py

--- Ph.py
+++ Ph.py
@@ -1,59 +1,110 @@
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import logging
 import config
-import telegram
-from random import randint
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
 TOKEN = config.token
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
+logger = logging.getLogger(__name__)
 
+CHOOSING, SELECTTEST, LEARNTOPIC, CHECKANSWER = range(4)
 
+def start(update, context) -> int:
+    reply_keyboard = [['Проверить себя', 'Разобраться в теме']]
 
-def start(update, context) -> None:
-    """Отправляет сообщение в ответ на команду /start."""
-    reply_keyboard =["Проверить себя", "Разобраться в теме"]
-    update.message.reply_text('Привет! Я помогу тебе разобраться в некоторых облостях физиологии',
-                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),)
-def hand(update, context):
+    update.message.reply_text(
+        'Привет! Я помогу тебе разобраться в некоторых областях физиологии\n'
+        'Что ты хочешь сделать',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
+
+    return CHOOSING
+
+def chooseTopic(update: Update, context: CallbackContext) -> int:
+    reply_keyboard = [['ЭКГ тест', 'Процессы возбуждения тест']]
+
+    update.message.reply_text(
+        'Выбери тему по которой тебе прислать задание '
+        'Отправь /menu чтобы вернуться\n\n',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
+    return SELECTTEST
+
+def testProcess(update: Update, context: CallbackContext) -> int:
     chat = update.effective_chat
-    if update.message.text == "Проверить себя":
-        reply_keyboard1 =[['ЭКГ тест'],['Процессы возбуждения тест'],[''],['d']]
-        update.message.reply_text('Выбери тему по которой тебе прислать задание',
-                                  reply_markup=ReplyKeyboardMarkup(reply_keyboard1, one_time_keyboard=True))
-    if update.message.text == "Разобраться в теме":
-        reply_keyboard2 = [['ЭКГ разбор'], ['b'], ['c'], ['d']]
-        update.message.reply_text('Выбери тему по которой тебе прислать объяснение',
-                                  reply_markup=ReplyKeyboardMarkup(reply_keyboard2, one_time_keyboard=True) )
-    if update.message.text == "ЭКГ тест":
-        a=0
-        #a=randint(0,10)
-        reply_keyboard3 = [['зубец P'], ['зубец T'], ['комлекс зубцов QRS'],['сегмент P-Q'],['сегмент S-T']]
-        if a==0:
-            updater.bot.send_photo(chat_id=chat.id, photo=open('ЭКГкар.jpg', 'rb'))
-            update.message.reply_text('Что соотвествует фазам деполяризации и быстрой реполяризациии потенциалов действия в кардиомиоцитах предсердий?',
-                                          reply_markup=ReplyKeyboardMarkup(reply_keyboard3, one_time_keyboard=True))
-            if update.message.text == "зубец P":
-                context.bot.send_message(chat_id=chat.id, text='Правильно!')
-            elif update.message.text != "зубец P":
-                context.bot.send_message(chat_id=chat.id, text='Ой, кажется ты ошибся. Вот разъяснение')
+    reply_keyboard = [['зубец P', 'зубец T', 'комлекс зубцов QRS', 'сегмент P-Q', 'сегмент S-T']]
+    context.bot.send_photo(chat_id=chat.id, photo=open('ЭКГкар.jpg', 'rb'))
+    update.message.reply_text(
+        'Что соотвествует фазам деполяризации и быстрой реполяризациии потенциалов действия в кардиомиоцитах предсердий?'
+        'Отправь /menu чтобы вернуться\n\n',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
 
+    return CHECKANSWER
 
+def checkCorrect(update: Update, context: CallbackContext) -> int:
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text='Правильно!')
+    return ConversationHandler.END
 
-    #else:
-     #   reply_keyboard = [["Проверить себя"], ["Разобраться в теме"]]
-      #  update.message.reply_text('Привет! Я помогу тебе разобраться в некоторых облостях физиологии',
-       #                       reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True), )
+def checkIncorrect(update: Update, context: CallbackContext) -> int:
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text='Ой, кажется ты ошибся. Вот разъяснение!')
+    context.bot.send_photo(chat_id=chat.id, photo=open('ЭКГоб1.jpg', 'rb'))
+    context.bot.send_photo(chat_id=chat.id, photo=open('ЭКГоб2.jpg', 'rb'))
+    return ConversationHandler.END
 
+def learnTopic(update: Update, context: CallbackContext) -> int:
+    reply_keyboard = [['ЭКГ разбор', 'a', 'b', 'c']]
 
+    update.message.reply_text(
+        'Выбери тему по которой тебе прислать объяснение '
+        'Отправь /menu чтобы вернуться\n\n',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
+    return LEARNTOPIC
 
+def showEcg(update: Update, context: CallbackContext) -> int:
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text='Что-то тут написано')
+    return ConversationHandler.END
 
+def main() -> None:
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
 
+    convHandler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            CHOOSING: [
+                MessageHandler(Filters.regex('^(Проверить себя)$'), chooseTopic),
+                MessageHandler(Filters.regex('^(Разобраться в теме)$'), learnTopic),
+            ],
+            SELECTTEST: [
+                MessageHandler(Filters.regex('^(ЭКГ тест)$'), testProcess),
+            ],
+            LEARNTOPIC: [
+                MessageHandler(Filters.regex('^(ЭКГ разбор)$'), showEcg),
+            ],
+            CHECKANSWER:[
+                MessageHandler(Filters.regex('^(зубец P)$'), checkCorrect),
+                MessageHandler(Filters.regex('^\w+'), checkIncorrect),
+            ]
+        },
+        fallbacks=[CommandHandler('menu', start)],
+    )
+    dispatcher.add_handler(convHandler)
+    updater.start_polling()
+    updater.idle()
 
-
-updater = Updater(TOKEN, use_context=True)
-
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(MessageHandler(Filters.all, hand))
-
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    main()
